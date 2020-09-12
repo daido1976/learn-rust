fe69ffdab782ef2fc05178c268869dcad1d6e509
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-09-12 18:19:10 +0900

    Compare the Guess to the Secret Number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -17,5 +18,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line!");
 
-    println!("You guessed: {}", guess)
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_num) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win"),
+    }
 }
