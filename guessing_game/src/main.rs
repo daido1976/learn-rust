28e867f58df23442130fc123fa02e8787b5e9166
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-09-13 19:05:29 +0900

    Allow Multiple Guesses with Looping

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,23 +8,30 @@ fn main() {
     println!("Guess the number!");
 
     let secret_num = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is {}", secret_num);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_num) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win"),
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
     }
 }
