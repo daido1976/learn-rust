2b0c6f4a1969bf6d54c5cd256cdd4f3ae4c246c7
Daido Shota <daido1976@gmail.com>
2020-10-02 23:55:20 +0900

    Parse CLI arguments with StructOpt

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,13 +1,15 @@
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::from_args();
 }
