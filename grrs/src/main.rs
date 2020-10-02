497907b517593985f0c05db417674db44ef3ae63
Daido Shota <daido1976@gmail.com>
2020-10-03 00:04:36 +0900

    First implementation of grrs

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -12,4 +12,11 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
