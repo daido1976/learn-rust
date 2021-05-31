03a25dda10363bca8eae47b25cbfec1cbcc6eb2e
Daido Shota <daido1976@gmail.com>
2021-05-31 23:54:02 +0900

    Impl hello world in todo_web

--- todo_web/src/main.rs
+++ todo_web/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer};
+
+#[get("/")]
+async fn index() -> Result<HttpResponse, actix_web::Error> {
+    let response_body = "Hello world";
+    Ok(HttpResponse::Ok().body(response_body))
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), actix_web::Error> {
+    HttpServer::new(move || App::new().service(index))
+        .bind("127.0.0.1:3000")?
+        .run()
+        .await?;
+    Ok(())
 }
