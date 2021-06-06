6acac05bc15ccb4947dd1eb3a11991e55a7efde6
daido1976 <daido1976@gmail.com>
2021-06-06 22:29:05 +0900

    Listen tcp connection

--- hello_http_server/src/main.rs
+++ hello_http_server/src/main.rs
@@ -1,3 +1,14 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let addr = "127.0.0.1:7878";
+    let listner = TcpListener::bind(addr).unwrap();
+    println!("start listening on {}", addr);
+
+    for stream in listner.incoming() {
+        let stream = stream.unwrap();
+
+        println!("{:?}", stream);
+        println!("connection established!")
+    }
 }
