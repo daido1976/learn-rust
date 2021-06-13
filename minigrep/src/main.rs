5f8051ebf302b6dcc989f1521613c5d152cd2e25
daido1976 <daido1976@gmail.com>
2021-06-13 12:08:07 +0900

    Create constructor for Config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,7 +2,7 @@ use std::{env, fs::File, io::Read};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     let mut file = File::open(config.file_name).expect("file not found!");
     let mut contents = String::new();
@@ -17,9 +17,11 @@ struct Config {
     file_name: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_name = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_name = args[2].clone();
 
-    Config { query, file_name }
+        Config { query, file_name }
+    }
 }
