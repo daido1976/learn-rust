6bb21fd4e0ad2d8b1c7949dbb12f5ffc1650a5e9
daido1976 <daido1976@gmail.com>
2021-06-13 11:35:27 +0900

    Save args into variables

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_name = &args[2];
+    println!("{:?}", query);
+    println!("{:?}", file_name);
 }
