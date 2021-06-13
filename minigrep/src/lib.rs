d0a63d53381c055114e439e28ef3efb200d1c20b
daido1976 <daido1976@gmail.com>
2021-06-13 15:56:34 +0900

    Write Code to Pass the Test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -26,3 +26,29 @@ impl Config {
         Ok(Config { query, file_name })
     }
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+
+    return results;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+}
