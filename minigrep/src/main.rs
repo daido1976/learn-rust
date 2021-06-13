12e9c9e505f11b51b0f8a0c5fae4e061b468c13a
daido1976 <daido1976@gmail.com>
2021-06-13 11:58:13 +0900

    Extract the argument parser

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,10 +2,7 @@ use std::{env, fs::File, io::Read};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_name = &args[2];
-    println!("{:?}", query);
-    println!("{:?}", file_name);
+    let (_query, file_name) = parse_config(&args);
 
     let mut file = File::open(file_name).expect("file not found!");
     let mut contents = String::new();
@@ -13,4 +10,10 @@ fn main() {
     file.read_to_string(&mut contents)
         .expect("something went wrong reading the file!");
     println!("{:?}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_name = &args[2];
+    (query, file_name)
 }
