ee1f558ebf54622a9947ef8cf23c7844b9acec18
daido1976 <daido1976@gmail.com>
2021-06-13 17:14:49 +0900

    Print Errors to Standard Error

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,14 +6,14 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     match minigrep::run(config) {
         Ok(_) => (),
         Err(e) => {
-            println!("Application error: {}", e);
+            eprintln!("Application error: {}", e);
             process::exit(1);
         }
     }
