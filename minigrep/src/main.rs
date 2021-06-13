8d8b511457d615c308059efdd85929db1201abba
daido1976 <daido1976@gmail.com>
2021-06-13 11:48:21 +0900

    Read a file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs::File, io::Read};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,4 +6,11 @@ fn main() {
     let file_name = &args[2];
     println!("{:?}", query);
     println!("{:?}", file_name);
+
+    let mut file = File::open(file_name).expect("file not found!");
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents)
+        .expect("something went wrong reading the file!");
+    println!("{:?}", contents);
 }
