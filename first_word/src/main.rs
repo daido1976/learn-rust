01da73e9fa4bef08dc1aa29e9b7d6bf74d9fc28d
daido1976 <daido1976@gmail.com>
2021-06-13 20:59:40 +0900

    Add test & refactor for first_word

--- first_word/src/main.rs
+++ first_word/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     println!("{:?}", word2);
 }
 
-fn first_word1(s: &String) -> &str {
+fn first_word1(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
@@ -14,11 +14,25 @@ fn first_word1(s: &String) -> &str {
             return &s[0..i];
         }
     }
-
-    &s[..]
+    s
 }
 
 fn first_word2(s: &str) -> &str {
-    let result = s.split_whitespace().nth(0);
+    let result = s.split_whitespace().next();
     result.unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_first_word1() {
+        let s = "hello world".to_string();
+        assert_eq!("hello".to_string(), first_word1(&s));
+    }
+    #[test]
+    fn test_first_word2() {
+        let s = "hello world".to_string();
+        assert_eq!("hello".to_string(), first_word2(&s));
+    }
 }
