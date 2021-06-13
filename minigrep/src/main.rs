feda6a50769cbaa17a187d04578d11116e4fd6ec
daido1976 <daido1976@gmail.com>
2021-06-13 12:05:30 +0900

    Group configuration values

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,9 +2,9 @@ use std::{env, fs::File, io::Read};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (_query, file_name) = parse_config(&args);
+    let config = parse_config(&args);
 
-    let mut file = File::open(file_name).expect("file not found!");
+    let mut file = File::open(config.file_name).expect("file not found!");
     let mut contents = String::new();
 
     file.read_to_string(&mut contents)
@@ -12,8 +12,14 @@ fn main() {
     println!("{:?}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_name = &args[2];
-    (query, file_name)
+struct Config {
+    query: String,
+    file_name: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_name = args[2].clone();
+
+    Config { query, file_name }
 }
