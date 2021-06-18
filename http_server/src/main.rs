b7d21c01854b33d3c4b568def4256a4ee8615ba2
daido1976 <daido1976@gmail.com>
2021-06-19 08:19:05 +0900

    Simulate a Slow Request in the Current Server Implementation

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs::File,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -22,11 +24,15 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     // for debug
-    println!("Request: {:?}", buffer); // prints bytes string in the buffer.
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
     let (status_line, file_path) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
     } else {
         ("HTTP/1.1 404 Not found\r\n\r\n", "404.html")
@@ -40,5 +46,4 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-    // https://doc.rust-jp.rs/book-ja/ch20-02-multithreaded.html から再開
 }
