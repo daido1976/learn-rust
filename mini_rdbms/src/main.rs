d4a2e221d4b62175ef260daed95f81436a91e779
daido1976 <daido1976@gmail.com>
2021-09-12 20:14:05 +0900

    Define DiskManager

--- mini_rdbms/src/main.rs
+++ mini_rdbms/src/main.rs
@@ -1,3 +1,27 @@
+use std::{fs::File, io, path::Path};
+
 fn main() {
     println!("Hello, world!");
+}
+
+pub struct DiskManager {
+    heap_file: File,
+    next_page: u64,
+}
+impl DiskManager {
+    pub fn new(data_file: File) -> io::Result<Self> {
+        unimplemented!()
+    }
+    pub fn open(data_file_path: impl AsRef<Path>) -> io::Result<Self> {
+        unimplemented!()
+    }
+    pub fn allocate_page(&mut self) -> PageId {
+        unimplemented!()
+    }
+    pub fn read_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
+        unimplemented!()
+    }
+    pub fn write_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
+        unimplemented!()
+    }
 }
