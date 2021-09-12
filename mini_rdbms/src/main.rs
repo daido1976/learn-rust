db3ffb6843af4dca5958a56ecfeb232095f9039f
daido1976 <daido1976@gmail.com>
2021-09-12 21:01:38 +0900

    Separate module into disk.rs

--- mini_rdbms/src/main.rs
+++ mini_rdbms/src/main.rs
@@ -1,56 +1,6 @@
-use std::{
-    fs::{File, OpenOptions},
-    io::{self, Read, Seek, SeekFrom, Write},
-    path::Path,
-};
+use mini_rdbms::disk::PageId;
 
 fn main() {
+    let _a = PageId(100).to_u64();
     println!("Hello, world!");
-}
-
-pub struct PageId(pub u64);
-
-impl PageId {
-    pub fn to_u64(self) -> u64 {
-        self.0
-    }
-}
-
-pub const PAGE_SIZE: usize = 4096;
-pub struct DiskManager {
-    heap_file: File,
-    next_page_id: u64,
-}
-impl DiskManager {
-    pub fn new(heap_file: File) -> io::Result<Self> {
-        let heap_file_size = heap_file.metadata()?.len();
-        let next_page_id = heap_file_size / PAGE_SIZE as u64;
-        Ok(Self {
-            heap_file,
-            next_page_id,
-        })
-    }
-    pub fn open(heap_file_path: impl AsRef<Path>) -> io::Result<Self> {
-        let heap_file = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(true)
-            .open(heap_file_path)?;
-        Self::new(heap_file)
-    }
-    pub fn allocate_page(&mut self) -> PageId {
-        let page_id = self.next_page_id;
-        self.next_page_id += 1;
-        PageId(page_id)
-    }
-    pub fn read_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
-        let offset = PAGE_SIZE as u64 * page_id.to_u64();
-        self.heap_file.seek(SeekFrom::Start(offset))?;
-        self.heap_file.read_exact(data)
-    }
-    pub fn write_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
-        let offset = PAGE_SIZE as u64 * page_id.to_u64();
-        self.heap_file.seek(SeekFrom::Start(offset))?;
-        self.heap_file.write_all(data)
-    }
 }
