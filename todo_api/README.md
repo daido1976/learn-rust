517765d4ebe39235916ff1fa122129af9ee0ad1d
daido1976 <daido1976@gmail.com>
2021-09-23 22:09:19 +0900

    Blueprint todo_api

--- todo_api/README.md
+++ todo_api/README.md
@@ -3,7 +3,7 @@
 Todo app JSON API.
 
 ```sh
-$ curl -X POST http://localhost:8080/todos/ | jq .
+$ curl -X GET http://localhost:8080/todos/ | jq .
 $ curl -X POST http://localhost:8080/todos/ -d '{ "title": "Test", "body": "bodyだよ" }' | jq .
 $ curl -X PUT http://localhost:8080/todos/1 -d '{ "title": "Test2", "body": "updateしたよ" }' | jq .
 $ curl -X DELETE http://localhost:8080/todos/1 | jq .
--- todo_api/src/main.rs
+++ todo_api/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{delete, get, patch, post, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn greet(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/todos")]
+async fn todo_index(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().body("unimplemented!")
+}
+
+#[post("/todos")]
+async fn todo_create(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().body("unimplemented!")
+}
+
+#[patch("/todos/{id}")]
+async fn todo_update(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().body("unimplemented!")
+}
+
+#[delete("/todos/{id}")]
+async fn todo_delete(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().body("unimplemented!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(greet)
+            .service(todo_index)
+            .service(todo_create)
+            .service(todo_update)
+            .service(todo_delete)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
