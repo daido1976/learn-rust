90da10bf50babefb8c4954348d3bcb9dadbe66af
daido1976 <daido1976@gmail.com>
2021-09-24 23:04:43 +0900

    Defining data structures

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,3 +1,36 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+const DB_PATH: &str = "sandbox.json";
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Pet {
+    id: usize,
+    name: String,
+    category: String,
+    age: usize,
+    created_at: DateTime<Utc>,
+}
+
+enum Event<I> {
+    Input(I),
+    Tick,
+}
+
+#[derive(Copy, Clone, Debug)]
+enum MenuItem {
+    Home,
+    Pets,
+}
+
+impl From<MenuItem> for usize {
+    fn from(input: MenuItem) -> usize {
+        match input {
+            MenuItem::Home => 0,
+            MenuItem::Pets => 1,
+        }
+    }
+}
 fn main() {
     println!("Hello, world!");
 }
