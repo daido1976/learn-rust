6acf8f2facdafbf485ede94f2958287bc01dc260
daido1976 <daido1976@gmail.com>
2021-10-08 02:57:58 +0900

    [Refactor] Explicitly handle command line arguments in yumemi

--- yumemi/src/main.rs
+++ yumemi/src/main.rs
@@ -6,25 +6,30 @@ use gen_csv::gen_csv;
 use std::{env, fs::create_dir_all, time::Instant};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let mut args = env::args();
     let file_path = "./data/game_score_log.csv";
 
-    if args.len() > 1 && &args[1] == "gen_csv" {
-        let lines: i32 = if args.len() > 2 {
-            // when passed line count from command line
-            args[2].parse().expect("can't parse to number")
-        } else {
-            // the default value
-            100
-        };
-        create_dir_all("data").expect("failed to create directory");
-        gen_csv(file_path, lines).expect("failed to generate csv");
-    } else {
-        let now = Instant::now();
-        run(file_path).expect("failed to run");
-        println!(
-            "time: {} seconds",
-            now.elapsed().as_millis() as f32 / 1000_f32
-        );
+    args.next();
+    match args.next() {
+        None => {
+            // when executed `$ cargo run`
+            let now = Instant::now();
+            run(file_path).expect("failed to run");
+            println!(
+                "time: {} seconds",
+                now.elapsed().as_millis() as f32 / 1000_f32
+            );
+        }
+        Some(first_arg) if first_arg == "gen_csv" => {
+            // when executed `$ cargo run gen_csv <lines>`
+            let lines: i32 = args
+                .next()
+                .unwrap_or_else(|| 100.to_string())
+                .parse()
+                .expect("failed to parse as a number");
+            create_dir_all("data").expect("failed to create directory");
+            gen_csv(file_path, lines).expect("failed to generate csv");
+        }
+        _ => println!("Usage: `cargo run` or `cargo run gen_csv <lines>`"),
     }
 }
