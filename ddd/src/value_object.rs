83994eb2212f04893b442654d6707664841654f2
daido1976 <daido1976@gmail.com>
2021-10-09 22:28:05 +0900

    Give type constraints to value object in ddd

--- ddd/src/value_object.rs
+++ ddd/src/value_object.rs
@@ -1,32 +1,64 @@
+use regex::Regex;
+use std::str::FromStr;
+
 #[derive(Clone, Debug, PartialEq)]
 pub struct FullName {
-    first_name: String,
-    last_name: String,
+    first_name: Name,
+    last_name: Name,
 }
 
 impl FullName {
-    pub fn new(first_name: &str, last_name: &str) -> Self {
+    pub fn new(first_name: Name, last_name: Name) -> Self {
         Self {
-            first_name: first_name.to_string(),
-            last_name: last_name.to_string(),
+            first_name,
+            last_name,
         }
     }
 
     pub fn first_name(&self) -> String {
-        self.first_name.clone()
+        self.first_name.0.clone()
     }
 
     pub fn last_name(&self) -> String {
-        self.last_name.clone()
+        self.last_name.0.clone()
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Name(String);
+
+impl FromStr for Name {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let regex = Regex::new(r#"^[a-zA-Z]+$"#).unwrap();
+        if regex.is_match(s) {
+            Ok(Name(s.to_string()))
+        } else {
+            Err("許可されていない文字が使われています".to_string())
+        }
     }
 }
 
 #[test]
-fn test_equality_of_value_object() {
-    let taro_tanaka_1 = FullName::new("taro", "tanaka");
-    let taro_tanaka_2 = FullName::new("taro", "tanaka");
-    let jiro_suzuki = FullName::new("jiro", "suzuki");
+fn show_names() {
+    let first_name = "taro".parse().unwrap();
+    let last_name = "tanaka".parse().unwrap();
+    let full_name = FullName::new(first_name, last_name);
 
-    assert_eq!(taro_tanaka_1, taro_tanaka_2);
-    assert_ne!(taro_tanaka_1, jiro_suzuki);
+    println!("full_name: {:?}", full_name);
+    println!("first_name: {:?}", full_name.first_name());
+    println!("last_name: {:?}", full_name.last_name());
+}
+
+#[test]
+fn test_parse_name() {
+    let valid_name = "taro".parse::<Name>();
+    assert!(valid_name.is_ok());
+
+    let invalid_name = "taro123".parse::<Name>();
+    assert!(invalid_name.is_err());
+
+    let invalid_name = "太郎".parse::<Name>();
+    assert!(invalid_name.is_err());
 }
