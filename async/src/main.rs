ccfd5365a385a527d3129a6979806781b96880a2
daido1976 <daido1976@gmail.com>
2021-11-21 18:28:01 +0900

    Hello tokio in async
    See. https://zenn.dev/magurotuna/books/tokio-tutorial-ja/viewer/hello_tokio

--- async/src/main.rs
+++ async/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> mini_redis::Result<()> {
+    let mut client = mini_redis::client::connect("127.0.0.1:6379").await?;
+    client.set("hello", "world".into()).await?;
+    let result = client.get("hello").await?;
+    println!("got value from the server; result={:?}", result);
+
+    Ok(())
 }
