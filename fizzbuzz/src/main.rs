1627e58c0cfd64b4fe01cd476a427fa631e93819
daido1976 <daido1976@gmail.com>
2022-01-27 23:43:40 +0900

    fizzbuzz2

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -20,6 +20,26 @@ fn fizzbuzz(n: usize) -> String {
     }
 }
 
+fn _fizzbuzz2() {
+    let fizzes = std::iter::repeat("")
+        .take(2)
+        .chain(std::iter::once("Fizz"))
+        .cycle();
+    let buzzes = std::iter::repeat("")
+        .take(4)
+        .chain(std::iter::once("Buzz"))
+        .cycle();
+    let fizzbuzzes = fizzes.zip(buzzes);
+    let fizzbuzz = (1..=100).zip(fizzbuzzes).map(|tupple| match tupple {
+        (i, ("", "")) => i.to_string(),
+        (_, (fizz, buzz)) => format!("{}{}", fizz, buzz),
+    });
+
+    for line in fizzbuzz {
+        println!("{}", line)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -35,5 +55,10 @@ mod tests {
     #[should_panic]
     fn test_fizzbuzz_given_zero() {
         fizzbuzz(0);
+    }
+
+    #[test]
+    fn test_fizzbuzz2() {
+        _fizzbuzz2();
     }
 }
