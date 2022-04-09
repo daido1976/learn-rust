1a08fc7350d73cd7be712b783c5728b261cb6e08
daido1976 <daido1976@gmail.com>
2022-04-09 20:43:12 +0900

    Refactor roman_to_int

--- leetcode/src/roman_to_int.rs
+++ leetcode/src/roman_to_int.rs
@@ -1,6 +1,14 @@
 use crate::Solution;
 
 // See. https://leetcode.com/problems/roman-to-integer/
+//
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII.
+// Instead, the number four is written as IV. Because the one is before the five we subtract it making four.
+// The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+//
+// I can be placed before V (5) and X (10) to make 4 and 9.
+// X can be placed before L (50) and C (100) to make 40 and 90.
+// C can be placed before D (500) and M (1000) to make 400 and 900.
 #[test]
 fn test_roman_to_int() {
     assert_eq!(Solution::roman_to_int("III".to_string()), 3);
@@ -11,62 +19,23 @@ fn test_roman_to_int() {
 // LeetCode に提出するのは以下のみ
 impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
-        const END_OF_CHARS: char = '0';
-        let mut result = 0;
-        let mut two_chars = false;
-        let next_char = |i| s.char_indices().nth(i + 1).map_or(END_OF_CHARS, |n| n.1);
-
-        for (i, c) in s.char_indices() {
-            if two_chars {
-                two_chars = false;
-                continue;
-            }
-
-            let n = match c {
-                'I' => {
-                    let next = next_char(i);
-                    if next == 'V' {
-                        two_chars = true;
-                        4
-                    } else if next == 'X' {
-                        two_chars = true;
-                        9
-                    } else {
-                        1
-                    }
-                }
+        s.chars().rfold(0, |acc, c| {
+            acc + match c {
+                // when next char V or X
+                'I' if acc >= 5 => -1,
+                'I' => 1,
                 'V' => 5,
-                'X' => {
-                    let next = next_char(i);
-                    if next == 'L' {
-                        two_chars = true;
-                        40
-                    } else if next == 'C' {
-                        two_chars = true;
-                        90
-                    } else {
-                        10
-                    }
-                }
+                // when next char L or C
+                'X' if acc >= 50 => -10,
+                'X' => 10,
                 'L' => 50,
-                'C' => {
-                    let next = next_char(i);
-                    if next == 'D' {
-                        two_chars = true;
-                        400
-                    } else if next == 'M' {
-                        two_chars = true;
-                        900
-                    } else {
-                        100
-                    }
-                }
+                // when next char D or M
+                'C' if acc >= 500 => -100,
+                'C' => 100,
                 'D' => 500,
                 'M' => 1000,
-                _ => panic!("Constraint violation."),
-            };
-            result += n;
-        }
-        result
+                _ => 0,
+            }
+        })
     }
 }
