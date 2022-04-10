21e0f4eb466c572aa5d1fa6dbd43eb630161fd86
daido1976 <daido1976@gmail.com>
2022-04-10 11:30:54 +0900

    Refactor search_insert

--- leetcode/src/search_insert.rs
+++ leetcode/src/search_insert.rs
@@ -15,9 +15,6 @@ fn test_two_sum() {
 // LeetCode に提出するのは以下のみ
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        match nums.binary_search(&target) {
-            Ok(n) => n as i32,
-            Err(n) => n as i32,
-        }
+        nums.binary_search(&target).unwrap_or_else(|x| x) as i32
     }
 }
