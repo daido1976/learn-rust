9d63e1b25ff646487181c5ef8ab1f17a1f35191d
daido1976 <daido1976@gmail.com>
2022-04-30 18:09:22 +0900

    zero2prod Chapter 3, Part 0
    https://www.lpalmieri.com/posts/2020-08-09-zero-to-production-3-how-to-bootstrap-a-new-rust-web-api-from-scratch/

--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
