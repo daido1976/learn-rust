6740d6e984d3e21d61b3f9cd7f50cbb314b754da
daido1976 <daido1976@gmail.com>
2022-05-01 19:07:52 +0900

    Add assertion & work sqlx's query! macro

--- zero2prod/tests/health_check.rs
+++ zero2prod/tests/health_check.rs
@@ -44,7 +44,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         "postgres://postgres:password@localhost/zero2prod",
     );
     let config = config::get_configuration();
-    let connection = PgConnection::connect(&config.database_url)
+    let mut connection = PgConnection::connect(&config.database_url)
         .await
         .expect("Failed to connect to Postgres.");
 
@@ -60,6 +60,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
