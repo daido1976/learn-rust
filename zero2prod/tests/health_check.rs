0b3d05a26041999582c6b9289d29dc8936092cc0
daido1976 <daido1976@gmail.com>
2022-05-01 23:46:40 +0900

    Refactor zero2prod test

--- zero2prod/tests/health_check.rs
+++ zero2prod/tests/health_check.rs
@@ -38,11 +38,9 @@ async fn spawn_app() -> TestApp {
 
 #[tokio::test]
 async fn health_check_works() {
-    // Arrange
     let app = spawn_app().await;
     let client = reqwest::Client::new();
 
-    // Act
     let response = client
         // Use the returned application address
         .get(&format!("{}/health_check", &app.address))
@@ -50,18 +48,15 @@ async fn health_check_works() {
         .await
         .expect("Failed to execute request.");
 
-    // Assert
     assert!(response.status().is_success());
     assert_eq!(response.content_length(), Some(0));
 }
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    // Arrange
     let app = spawn_app().await;
     let client = reqwest::Client::new();
 
-    // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
         .post(&format!("{}/subscriptions", &app.address))
@@ -71,8 +66,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .await
         .expect("Failed to execute request.");
 
-    // Assert
-    assert_eq!(200, response.status().as_u16());
+    assert_eq!(response.status().as_u16(), 200);
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
         .fetch_one(&app.db_pool)
@@ -85,7 +79,6 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
-    // Arrange
     let app = spawn_app().await;
     let client = reqwest::Client::new();
     let test_cases = vec![
@@ -95,7 +88,6 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     ];
 
     for (invalid_body, error_message) in test_cases {
-        // Act
         let response = client
             .post(&format!("{}/subscriptions", &app.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
@@ -104,10 +96,9 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             .await
             .expect("Failed to execute request.");
 
-        // Assert
         assert_eq!(
-            400,
             response.status().as_u16(),
+            400,
             // Additional customised error message on test failure
             "The API did not fail with 400 Bad Request when the payload was {}.",
             error_message
