94dc1d3b3b34c524fdf7793bc2075971f3be9286
daido1976 <daido1976@gmail.com>
2022-05-01 10:54:35 +0900

    Add subscriptions table

--- zero2prod/migrations/20220501015327_create_subscriptions_table.sql
+++ zero2prod/migrations/20220501015327_create_subscriptions_table.sql
@@ -1,1 +1,7 @@
--- Add migration script here
+CREATE TABLE subscriptions(
+   id uuid NOT NULL,
+   PRIMARY KEY (id),
+   email TEXT NOT NULL UNIQUE,
+   name TEXT NOT NULL,
+   subscribed_at timestamptz NOT NULL
+);
