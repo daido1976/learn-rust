376c3c3820c044dacf37225e95e5311d70e4cdc0
daido1976 <daido1976@gmail.com>
2023-02-06 21:17:02 +0900

    Add sqlite-sys as sub-crate to ffi
    See. https://doc.rust-lang.org/cargo/reference/specifying-dependencies.html#specifying-path-dependencies

--- ffi/src/bin/sqlite_sys.rs
+++ ffi/src/bin/sqlite_sys.rs
@@ -1,11 +1,4 @@
-use std::os::raw::{c_char, c_int};
-
-// On macOS, it is bundled by default.
-#[link(name = "sqlite3")]
-extern "C" {
-    pub fn sqlite3_libversion() -> *const c_char;
-    pub fn sqlite3_libversion_number() -> c_int;
-}
+use sqlite_sys::*;
 
 fn main() {
     unsafe {
